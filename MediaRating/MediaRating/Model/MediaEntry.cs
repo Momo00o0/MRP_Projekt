ff75fada5320d9618f27686fc9658035eb66c20a
Mohamad Aldulemi <mohamadaldulemi@gmail.com>
2025-09-15 01:08:07 +0200

    Tests

---
+++ MediaRating/MediaRating/Model/MediaEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaRating.Model
+{
+    public abstract class MediaEntry
+    {
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Fügen Sie ggf. den „erforderlichen“ Modifizierer hinzu, oder deklarieren Sie den Modifizierer als NULL-Werte zulassend.
+        public MediaEntry() { }
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Fügen Sie ggf. den „erforderlichen“ Modifizierer hinzu, oder deklarieren Sie den Modifizierer als NULL-Werte zulassend.
+        protected MediaEntry(string title, string description, int releaseYear, int ageRestriction, User creator, double averageScore)
+        {
+            Title = title;
+            Description = description;
+            ReleaseYear = releaseYear;
+            AgeRestriction = ageRestriction;
+            Creator = creator;
+            AverageScore = averageScore;
+        }
+        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ReleaseYear { get; set; }
+        public List<string> Genres { get; set; } = new();
+        public int AgeRestriction { get; set; }
+
+        public User Creator { get; }
+        public List<Rating> Ratings { get; set; } = new();
+        public List<User> FavoritedBy { get; set; } = new();
+
+        public double AverageScore { get; set; }
+
+    }
+}
---
+++ MediaRating/MediaRating/Model/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaRating.Model
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Movie MyFilm = new Movie();
+            MyFilm.Title = "King Kong";
+            MyFilm.Description = "Scary";
+            MyFilm.ReleaseYear = 2017;
+            Console.WriteLine($"{MyFilm.Title} {MyFilm.Description} {MyFilm.ReleaseYear}");
+
+        }
+    }
+}
