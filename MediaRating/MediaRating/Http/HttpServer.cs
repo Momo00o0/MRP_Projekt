a3c6a2e4d197de9a6259dd32ebd29bc85af5c4ea
Mohamad Aldulemi <mohamadaldulemi@gmail.com>
2025-10-19 02:03:15 +0200

    HttpListener + Router + GET /api/health

---
+++ MediaRating/MediaRating/Http/HttpServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaRating.Http
+{
+    public class HttpServer
+    {
+        private readonly HttpListener _listener = new();
+        private readonly Func<HttpListenerContext, Task> _handler;
+
+        public HttpServer(string prefix, Func<HttpListenerContext, Task> handler)
+        {
+            _listener.Prefixes.Add(prefix);
+            _handler = handler;
+        }
+
+        public async Task RunAsync()
+        {
+            _listener.Start();
+            Console.WriteLine($"Listening on {string.Join(", ", _listener.Prefixes)}");
+            while (true)
+            {
+                var ctx = await _listener.GetContextAsync();
+                _ = Task.Run(() => _handler(ctx));
+            }
+        }
+    }
+}
